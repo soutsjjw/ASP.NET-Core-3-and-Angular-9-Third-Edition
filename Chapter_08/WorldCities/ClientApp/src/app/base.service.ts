0ceef52cb7f3a9b7592cb57a41664a301f976563
souts <souts.jjw@gmail.com>
2020-11-15 19:20:02 +0800

    Chapter_08

---
+++ Chapter_08/WorldCities/ClientApp/src/app/base.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class BaseService {
+  constructor(
+    protected http: HttpClient,
+    protected baseUrl: string
+  ) { }
+
+  abstract getData<ApiResult>(
+    pageIndex: number,
+    pageSize: number,
+    sortColumn: string,
+    sortOrder: string,
+    filterColumn: string,
+    filterQuery: string) : Observable<ApiResult>;
+
+  abstract get<T>(id: number): Observable<T>;
+  abstract put<T>(item: T): Observable<T>;
+  abstract post<T>(item: T): Observable<T>;
+}
+
+export interface ApiResult<T> {
+  data: T[];
+  pageIndex: number;
+  pageSize: number;
+  totalCount: number;
+  totalPages: number;
+  sortColumn: string;
+  sortOrder: string;
+  filterColumn: string;
+  filterQuery: string;
+}
